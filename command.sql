a23dacff7bdc436b2fdd9b8a70e1a13bda1e0b2d
Priyanka Maheshwari <priyankamaheshwari.2021@gmail.com>
2026-04-24 22:51:22 +0200

    created dim and fact model

---
+++ command.sql
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Exit if error
+set -e
+
+echo "Creating project folder..."
+mkdir -p DBT-project
+cd DBT-project
+
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+echo "Upgrading pip..."
+pip install --upgrade pip
+
+pip install dbt-snowflake==1.9.0
+
+echo "Installing dbt..."
+pip install dbt-core
+
+echo "Freezing dependencies..."
+pip freeze > requirements.txt
+
+echo "Setup complete!"
+
+--create dbt profile
+--mac
+mkdir ~/.dbt
+
+--create profiles.yml file in the DBT-projects /Users/priyankamaheswari/.dbt
+--Need to putl all the information same as we put in our snowflake account
+
+-- only want to run one model
+-- dbt run --model dim_users
